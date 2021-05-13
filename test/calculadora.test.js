bfa78f99ee7d8d179419620586a31d68c7c0788b
Juan Mejia <juan.mejia1594@gmail.com>
2021-05-13 10:28:34 -0500

    prueba de la funcion suma

---
+++ test/calculadora.test.js
@@ -0,0 +1,12 @@
+const {test, expect} = require('@jest/globals')
+
+test('la suma de 2 y 5 devuelve 7', () => {
+    // 2 + 5 =7
+    let a = 2;
+    let b = 5;
+    let esperado = 7;
+    // act
+    let resultado = suma(a, b);
+    // assert
+    expect(resultado).toBe(esperado);
+})
