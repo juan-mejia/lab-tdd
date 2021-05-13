398b8bc335ac5d8136dbbca4e3f53a523e2c79e0
Juan Mejia <juan.mejia1594@gmail.com>
2021-05-13 10:31:14 -0500

    funcoin suma creada y funcionando

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,4 +1,5 @@
 const {test, expect} = require('@jest/globals')
+const suma = require('../modules/calculadora');
 
 test('la suma de 2 y 5 devuelve 7', () => {
     // 2 + 5 =7
@@ -6,7 +7,7 @@ test('la suma de 2 y 5 devuelve 7', () => {
     let b = 5;
     let esperado = 7;
     // act
-    let resultado = suma(a, b);
+    let resultado = suma(a,b);
     // assert
     expect(resultado).toBe(esperado);
 })
