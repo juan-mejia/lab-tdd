71550928da586a38e5fa3ee03efc66f492750c60
Juan Mejia <juan.mejia1594@gmail.com>
2021-05-25 10:06:40 -0500

    test de resta creado

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,5 +1,5 @@
 const {test, expect} = require('@jest/globals')
-const suma = require('../modules/calculadora');
+const suma = require('../modules/suma');
 
 test('la suma de 2 y 5 devuelve 7', () => {
     // 2 + 5 =7
@@ -10,4 +10,15 @@ test('la suma de 2 y 5 devuelve 7', () => {
     let resultado = suma(a,b);
     // assert
     expect(resultado).toBe(esperado);
+})
+
+test('la resta de 10 y 6 devuelva 4', ()=> {
+    // 10 - 6 = 4
+    let a = 10
+    let b = 6
+    let esperado = 4
+    // act
+    let resultado = resta(a,b)
+    // assert 
+    expect(resultado).toBe(esperado)
 })
